019c78252a2dfe85b7fbbf49a10c34ae992f2a49
ksmai <kasingmai@gmail.com>
2018-02-11 22:59:24 +0800

    Add karma testing configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  selector: 'landing-app',
 })
 export class AppComponent {
 }
