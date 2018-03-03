48bb8e05446e38df99d2a54d2e9121618b4c2c8e
ksmai <kasingmai@gmail.com>
2018-03-03 22:57:13 +0800

    Add modules to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'font-awesome/scss/font-awesome.scss';
 
-import { AppComponent } from './app.component';
 import '../styles/styles.scss';
+import { AppComponent } from './app.component';
+import { NavbarModule } from './navbar/navbar.module';
+import { SplashModule } from './splash/splash.module';
+import { IntroductionModule } from './introduction/introduction.module';
+import { FeaturesModule } from './features/features.module';
+import { ShowcaseModule } from './showcase/showcase.module';
+import { ContactModule } from './contact/contact.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+
+    NavbarModule,
+    SplashModule,
+    IntroductionModule,
+    FeaturesModule,
+    ShowcaseModule,
+    ContactModule,
+    CoreModule,
   ],
 
   declarations: [
